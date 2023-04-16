6d8a6650ecd4cfe0b0f395469ec4c15259495c48
mak.skid <skid12261@gmail.com>
2023-04-16 20:27:42 +0900

    further improvement on the current time function

--- json_asset.py
+++ json_asset.py
@@ -5,16 +5,16 @@ import bisect
 def current_time():
     JST = timezone(timedelta(hours=+9), 'JST')
 
-    def helper(now_minute=datetime.now(JST).minute):
-        freq = [3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53, 58]
-        i = bisect.bisect_left(freq, now_minute)
-        if i == 0:
-            return -1, freq[-1]-1
-        else:
-            return 0, freq[i-1]-1
+    now_minute=datetime.now(JST).minute
+    freq = [3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53, 58]
+    i = bisect.bisect_left(freq, now_minute)
+    if i == 0:
+        fetched_time = -1, freq[-1]-1
+    else:
+        fetched_time = 0, freq[i-1]-1
 
-    preformatted = datetime.now(JST) + timedelta(hours=helper()[0])
-    current_time = str(preformatted.strftime('%Y%m%d%H')) + str(helper()[1]).zfill(2)
+    preformatted = datetime.now(JST) + timedelta(hours=fetched_time[0])
+    current_time = str(preformatted.strftime('%Y%m%d%H')) + str(fetched_time[1]).zfill(2)
 
     return current_time
 
