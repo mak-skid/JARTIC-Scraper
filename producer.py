77d2f433789dfb058ce22fcd57ce0fd8171ebd31
mak.skid <skid12261@gmail.com>
2023-04-18 22:55:22 +0900

    mainly an update of producer.py

--- producer.py
+++ producer.py
@@ -1,11 +1,33 @@
-from json_asset import scrape
+from scraper import scrape, current_time
+from area_dict import area
 
-def producer():
-    raw_json = scrape('C03')
+current_time = current_time()
 
-    events = {}
+def producer(): #iterating each area in area_dict and returns a dictionary of all areas
+    all_areas = {}
+
+    for area_name in area.values():
+        area_code = area[area_name]
+        all_areas[area_code] = json_rearranger(area_code)
+    
+    return current_time, all_areas
+
+def json_rearranger(area_code): #fetch json and rearrange each element with an updated key
+    raw_json = scrape(area_code, current_time)
+
+    new_event = {}
+    new_events = [] #list of updated JSON of each traffic event
+
+    keys = ['c', 'd', 'i', 'r', 'rd', 'j']
 
     for feature in raw_json["features"]:
-        events.append(feature["properties"])
+        event = feature["properties"]
+        for key, element in event.items():
+            if key in keys:
+                new_key = ['規制原因', '方向', '区間', '路線名', '規制内容', '渋滞長'][keys.index(key)]
+                new_event[new_key] = element
+        new_events.append(new_event)
 
-    return events
+
+    return new_events
+
