42767e65dba078f377a1da22644958b6502513c6
mak.skid <skid12261@gmail.com>
2023-05-01 23:47:21 +0900

    added coordinates to the json string

--- producer.py
+++ producer.py
@@ -18,16 +18,17 @@ def json_rearranger(area_code): #fetch json and rearrange each element with an u
     new_event = {}
     new_events = [] #list of updated JSON of each traffic event
 
-    keys = ['c', 'd', 'i', 'r', 'rd', 'j']
+    keys = ['c', 'd', 'i', 'r', 'rd', 'j', 'coordinates']
 
     for feature in raw_json["features"]:
         event = feature["properties"]
+        if feature["geometry"]:
+            event["coordinates"] = feature["geometry"]["coordinates"]
         for key, element in event.items():
             if key in keys:
-                new_key = ['規制原因', '方向', '区間', '路線名', '規制内容', '渋滞長'][keys.index(key)]
+                new_key = ['規制原因', '方向', '区間', '路線名', '規制内容', '渋滞長', "位置"][keys.index(key)]
                 new_event[new_key] = element
         new_events.append(new_event)
-
 
     return new_events
 
